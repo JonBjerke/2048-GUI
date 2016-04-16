8c89f30e9978071ca9e09a4f489456414dc86c1e
flourtowel <jbjerke1@gmail.com>
2016-04-16 16:24:27 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,27 +11,50 @@ pub mod cursor ;
 pub mod clap ;
 mod grid ;
 pub mod frame ;
+
 //
 use conrod::{Canvas, Theme, Widget, color};
-use piston_window::{EventLoop, Glyphs, PistonWindow, UpdateEvent, WindowSettings};
+use piston_window::{EventLoop, Glyphs, PistonWindow, UpdateEvent, WindowSettings, clear};
 //
 
 pub use common::{ Dir, Evolution, Seed } ;
 pub use grid::{ Cell, Grid } ;
 
-//
+/*
+// Will be needed for game implementation
 type Backend = (<piston_window::G2d<'static> as conrod::Graphics>::Texture, Glyphs);
 type Ui = conrod::Ui<Backend>;
 type UiCell<'a> = conrod::UiCell<'a, Backend>;
 //
+*/
 
 /// Entry point.
 fn main() {
-  /*/HERE 
+ 
+
+ /*
+  // test code 1.0 gets ready to build a window but doesn't draw it 
   let window: PistonWindow =
         WindowSettings::new("Canvas Demo", [800, 600])
-            .exit_on_esc(true).vsync(true).build().unwrap();
-  //TO HERE*/
+            .exit_on_esc(true)
+            .vsync(true)
+            .build()
+            .unwrap();
+ */
+
+    // ***THIS ONE WORKS***
+    // Construct the window.
+    // Creates a new window and draws the border
+  let window: PistonWindow = WindowSettings::new("2048", (640, 480))
+    .exit_on_esc(true)
+    .build()
+    .unwrap_or_else(|e| { panic!("Failed to build PistonWindow: {}", e) });
+  for e in window {
+    e.draw_2d(|_c, g| {
+      clear([0.5, 1.0, 0.5, 1.0], g);
+    });
+  }
+
   use std::process::exit ;
 
   // Getting seed and painter from command line arguments.
@@ -44,5 +67,5 @@ fn main() {
   } ;
 
   frame::rendering_loop_user(seed, painter)
-}
 
+}
