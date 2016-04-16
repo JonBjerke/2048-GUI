d71f3a097bdc1914e213c7bdfceada8a3ce56ba0
Jonathan Light <jthgil12@users.noreply.github.com>
2016-04-16 14:36:05 -0500

    Added files via upload

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,35 @@
 extern crate ansi_term as ansi ;
 extern crate rand ;
 #[macro_use] extern crate conrod;
+extern crate piston_window;
+
 
 mod common ;
 pub mod cursor ;
 pub mod clap ;
 mod grid ;
 pub mod frame ;
+//
+use conrod::{Canvas, Theme, Widget, color};
+use piston_window::{EventLoop, Glyphs, PistonWindow, UpdateEvent, WindowSettings};
+//
 
 pub use common::{ Dir, Evolution, Seed } ;
 pub use grid::{ Cell, Grid } ;
 
+//
+type Backend = (<piston_window::G2d<'static> as conrod::Graphics>::Texture, Glyphs);
+type Ui = conrod::Ui<Backend>;
+type UiCell<'a> = conrod::UiCell<'a, Backend>;
+//
+
 /// Entry point.
 fn main() {
+  /*/HERE 
+  let window: PistonWindow =
+        WindowSettings::new("Canvas Demo", [800, 600])
+            .exit_on_esc(true).vsync(true).build().unwrap();
+  //TO HERE*/
   use std::process::exit ;
 
   // Getting seed and painter from command line arguments.
