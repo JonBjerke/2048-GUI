ef695a6c98bf30ecb30561172c6145eb0c595f98
flourtowel <jbjerke1@gmail.com>
2016-04-28 13:52:10 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -50,6 +50,49 @@ fn color_of_pow(pow: u32) -> Color {
   }
 }
 
+////https://github.com/PistonDevelopers/image
+
+// pub use std::path::Path;
+
+// fn path_of_image(pow: u32) -> Path {
+
+//   let pathEmpty = Path::new("pics/empty.png");
+//   let path2 = Path::new("pics/2.png");
+//   let path128 = Path::new("pics/128.png");
+//   let path8192 = Path::new("pics/8192.png");
+
+//   match pow {
+//     0 => pathEmpty,
+//     1 => path2,
+//     2 => path128,
+//     _ => path8192,
+//   }
+// }
+
+// extern crate image;
+
+// use std::fs::File;
+// use std::path::Path;
+
+// use image::GenericImage;
+
+// fn main() {
+//     // Use the open function to load an image from a Path.
+//     // ```open``` returns a dynamic image.
+//     let img = image::open(&Path::new("test.jpg")).unwrap();
+
+//     // The dimensions method returns the images width and height
+//     println!("dimensions {:?}", img.dimensions());
+
+//     // The color method returns the image's ColorType
+//     println!("{:?}", img.color());
+
+//     let ref mut fout = File::create(&Path::new("test.png")).unwrap();
+
+//     // Write the contents of this image to the Writer in PNG format.
+//     let _ = img.save(fout, image::PNG).unwrap();
+// }
+
 fn pow_of(grid: & Grid, row: usize, col: usize) -> u32 {
   match grid.grid()[row][col] {
     None => 0,
@@ -106,6 +149,7 @@ fn read_user_input() -> Dir {
 }
 
 fn main() {
+
   // Just like in minecraft, randomness is dictated by a seed.
   // This creates a random seed.
   let seed = Seed::mk() ;
@@ -128,32 +172,32 @@ fn main() {
   let mut grid = Grid::mk(seed) ;
   grid.spawn() ;
 
-  //let mut count = 0 ;
+  // let mut count = 0 ;
 
   // Rendering loop.
   'rendering: loop {
     use Dir::* ;
     use Evolution::* ;
 
-    //count = (count + 1) % 1000 ;
+    // count = (count + 1) % 1000 ;
 
-    //println!("count: {}", count) ;
+    // println!("count: {}", count) ;
   
     display_grid(& grid, & mut window) ;
 
 
     
-    // grid.right(); 
-    // //grid.spawn(); 
-    // grid.left(); 
+    grid.right(); 
+    //grid.spawn(); 
+    grid.left(); 
     
-    // grid.right(); 
+    grid.right(); 
     
-    // grid.left(); 
+    grid.left(); 
    
-    // grid.right(); 
+    grid.right(); 
    
-    // grid.up();
+    grid.up();
     
     // match count {
     //   250 => { grid.up() ; () },
@@ -163,39 +207,6 @@ fn main() {
     //   _ => (),
     // }
 
-    // 1) display new grid 
-    // 2) ask for user input
-    // 3) the game calculates new score and tile locations
-    // 4) loop ends
-    // (could put 1 at the end once all other parts are implemented.)
-
-    // // What is the user asking you to do?
-    // let evolution = match read_user_input() {
-    // let evolution = match AI{
-    //   Up => grid.up(),
-    //   Dw => grid.down(),
-    //   Rg => grid.right(),
-    //   Lf => grid.left(),
-    // } ;
-
-    // // The evolution tells you what happened.
-    // match evolution {
-    //   // Nothing happened (no move no merge).
-    //   Nothing => (),
-    //   // Some tiles moved but no merge.
-    //   Moved => grid.spawn(),
-    //   // Merged some tiles, yields the score of the tiles merged.
-    //   Merged(_score) => grid.spawn(),//possible animation
-    // }
-  }
-}
-
-  // Rendering loop.
-  'rendering: loop {
-    use Dir::* ;
-    use Evolution::* ;
-  
-    display_grid(& grid, & mut window) ;
     // 1) display new grid 
     // 2) ask for user input
     // 3) the game calculates new score and tile locations
